bfa04edc05670fdb0c9b545ccbec8f82d29f4958
SolarOven <54403528+SolarOven@users.noreply.github.com>
2019-09-08 18:19:29 +0800

    Update python对齐打印.py

--- python对齐打印.py
+++ python对齐打印.py
@@ -1,23 +1,41 @@
+__author__="SolarOven"
+__version__==2.0
+
 '''
 用法：
-puu(x),x是字符串
+puu(x),x是字符串,或者是一组用逗号隔开的变量
 boo(),把刚才你所有puu的x全部输出，上下对齐。
 
 usage：
-puu(x),x is a string separated by spaces
+puu(x),x is a string separated by spaces,or a group of variables separated with ','
 boo(),printf what you just puu all. up and down the aligenment
+
+例如：
+for example:
+
+    n='xxx'
+    s='?'
+    a='11'
+    puu("name sex age")
+    puu(n,s,a)
+    boo()
+    >>
+        name sex age
+        xxx  ?   11
+
+差不多就是这样。。
+almost like that..
 '''
 
-'''
-简称解释：比如“sss kkk”其中，sss就是一个需对齐元素，一下简称齐素
-'''
+
+#简称解释：比如“sss kkk”其中，sss就是一个需对齐元素，一下简称齐素
 n=-1#n为索引而非序列
 z=[]#数组型不用global也能在函数中修改
     #z=[行[齐素     ]]
 l={} #l记录此列需要多宽
 s_l={} #记录每个齐素的长度
 
-def puu(a):
+def disolve(a):
     global n
     n+=1
     z.append([])
@@ -57,10 +75,17 @@ def puu(a):
             s_len+=1
             elem+=i[1]
 
-
-
 def boo(x=' '):
     for h in z:
         for elem in enumerate(h):
             print(elem[1],end=(x+' '*(l[elem[0]]-s_l[elem[1]])))
         print('')
+
+def puu(*a):
+    if len(a)==1 and isinstance(a[0],str):
+        disolve(a[0])
+    else:
+        h=''
+        for i in a:
+            h+=(str(i)+' ')
+        disolve(h)
